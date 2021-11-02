ce984a8a5c634f116e54c2313b4e0212989565d7
YuliyaMikhaleva <umihaleva93@mail.ru>
2021-11-02 10:42:13 +0500

    Созданы и отстилизованы компоненты modalBasket, modalText

--- test_project/src/components/ModalBasket/Modal_basket.vue
+++ test_project/src/components/ModalBasket/Modal_basket.vue
@@ -1,33 +1,24 @@
 <!--Всплывающее окно корзины-->
 <template>
-    <div style="display: flex; justify-content: flex-end">
-        <div class="modal-dialog header_basket">
-            <div class="modal-content ">
-                <div class="header_basket_content">
-                    <div class="header_basket_content_top">
-                        <h5 class="header_basket_content_top_title">Оформить заказ</h5>
-                        <button class="header_basket_content_top_btn" type="button" data-bs-dismiss="modal">
+    <div :class="$style.modal">
+        <div :class="$style.modal__dialog" class="modal-dialog header_basket">
+            <div :class="$style.modal__content" class="modal-content ">
+                <div :class="$style.basket">
+                    <header :class="$style.basket__header">
+                        <h5 :class="$style.basket__title">Оформить заказ</h5>
+                        <button :class="$style.basket__btn" type="button" data-bs-dismiss="modal">
                             <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M20 2.01429L17.9857 0L10 7.98571L2.01429 0L0 2.01429L7.98571 10L0 17.9857L2.01429 20L10 12.0143L17.9857 20L20 17.9857L12.0143 10L20 2.01429Z" fill="black"/>
                             </svg>
                         </button>
-                    </div>
-                    <div  v-if="this.$store.getters.getBasket.length>0" >
-                        <span class="header_basket_content_text">В корзине:</span>
+                    </header>
+                    <section  v-if="this.$store.getters.getBasket.length>0" >
+                        <span :class="$style.basket__text">В корзине:</span>
 <!--                        здесь будет корзина с товарами-->
                         <Basket/>
-
                         <Form/>
-
-                    </div>
-                    <div v-if="this.$store.getters.getBasket.length<=0 && !showResultOrder" class="header_basket_content_resultOrder">
-                        <img class="header_basket_content_resultOrder_img" src="../../assets/img/basket.svg" width="99" height="99" alt="basket">
-                        <span class="header_basket_content_resultOrder_txt">В корзине ничего нет</span>
-                    </div>
-                    <div v-if="showResultOrder" class="header_basket_content_resultOrder">
-                        <img class="header_basket_content_resultOrder_img" src="../../assets/img/order.png" width="140" height="109" alt="order">
-                        <span class="header_basket_content_resultOrder_txt">Заказ успешно создан</span>
-                    </div>
+                    </section>
+                    <ModalText :class="$style.basket__modaltext" :text="title"/>
                 </div>
             </div>
         </div>
@@ -37,16 +28,30 @@
 <script>
     import Basket from "../Basket/Basket";
     import Form from "../Form/Form";
+    import ModalText from "../ModalText/ModalText";
     export default {
         name: "Modal_basket",
-        components: {Form, Basket},
+        components: {ModalText, Form, Basket},
         data(){
             return {
                 showParametrs:true,
                 showResultOrder: false,
             }
         },
+        computed:{
+            title(){
+                if (this.$store.getters.getBasket.length<=0 && !this.showResultOrder){
+                    return "В корзине ничего нет"
+                } else if (this.showResultOrder){
+                    return "Заказ успешно создан"
+                } else {
+                    return ""
+                }
+            }
         }
+
+
+    }
 </script>
 
 <style lang="scss" module>
---
+++ test_project/src/components/ModalText/ModalText.vue
@@ -0,0 +1,18 @@
+<template>
+    <section :class="$style['modal-text']">
+        <img v-if="text==='В корзине ничего нет'" src="../../assets/img/basket.svg" width="99" height="99" alt="basket">
+        <img v-if="text==='Заказ успешно создан'" class="header_basket_content_resultOrder_img" src="../../assets/img/order.png" width="140" height="109" alt="order">
+        <span :class="$style['modal-text__txt']">{{ text }}</span>
+    </section>
+</template>
+
+<script>
+    export default {
+        name: "ModalText",
+        props:['text']
+    }
+</script>
+
+<style lang="scss" module>
+@import "ModalText.module";
+</style>
