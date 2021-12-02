90c4b175ac0b82c684e904e57e22eff667e995df
YuliyaMikhaleva <umihaleva93@mail.ru>
2021-12-02 17:24:05 +0500

    Скорректирован компонент StarRating.vue

--- test_project/src/components/StarRating/StarRating.vue
+++ test_project/src/components/StarRating/StarRating.vue
@@ -1,27 +1,40 @@
 <!--Компонент рейтинга в виде звёзд-->
 <template>
     <div class="star-rating">
-        <div class="star-rating__wrap">
-            <Star v-bind:key="count" :count="count" v-for="count of maxCount"/>
-        </div>
+        <form class="star-rating__wrap">
+            <template v-for="count in array">
+                <input :key="'input-'+count" @click="addMark(count)" class="star-rating__input" :id="'star-rating-'+count" type="radio" name="rating" :value="count">
+                <label :key="'label-'+count" class="star-rating__ico fa fa-star-o fa-lg" :for="'star-rating-'+count" :title="count + ' out of 5 stars'"></label>
+            </template>
+        </form>
     </div>
 </template>
 
 <script>
-    import Star from "../Star/Star";
+
     export default {
         name: "StarRating",
-        components: {Star},
         data(){
             return {
                 maxCount:5,
+                array:[],
             }
         },
         methods:{
             addMark(count){
-                this.$emit('update',count)
+                this.$emit('update',count);
+
             },
         },
+        mounted() {
+            let x = [];
+            for (let i=1; i<=this.maxCount; i++){
+                x = [...x, i]
+            }
+            this.array = x.reverse()
+            console.log(this.array)
+        }
+
     }
 </script>
 
